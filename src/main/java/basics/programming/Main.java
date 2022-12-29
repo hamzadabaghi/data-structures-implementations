2672ac19a8f61e326128a6b5edff23dbc9416afd
Hamza DABAGHI <hamza.dabaghi@gmail.com>
2022-12-30 00:06:04 +0100

    [BT] : reformat code and merge LinkedList to the main branch.

--- src/main/java/basics/programming/Main.java
+++ src/main/java/basics/programming/Main.java
@@ -1,7 +1,6 @@
 package basics.programming;
 
 import basics.programming.arrays.Array;
-
 import basics.programming.linkedlists.LinkedList;
 
 public class Main {
@@ -9,20 +8,22 @@ public class Main {
 
         /*--------------- Arrays ---------------*/
         Array<Integer> array = new Array<Integer>();
-        Integer[] intersectionArray = {10,17,34};
+        Integer[] intersectionArray = {10, 17, 34};
         array.insert(30);
         array.insert(10);
         array.insert(20);
         array.insert(33);
         array.insert(17);
         array.insert(1);
-        array.insertAt(13,5);
+        array.insertAt(13, 5);
         System.out.println(array);
         array.reverse();
         System.out.println(array);
         System.out.println(array.size());
-        System.out.println(array.intersect(intersectionArray,3));
+        System.out.println(array.intersect(intersectionArray, 3));
         array.removeAt(1);
+
+        /*--------------- LinkedLists ---------------*/
 
         LinkedList<Integer> list = new LinkedList<>();
         list.addFirst(5);

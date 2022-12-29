fc71d8e24ba26ae550ee6a232fa9f12aad9d08f1
Hamza DABAGHI <hamza.dabaghi@gmail.com>
2022-12-30 00:03:36 +0100

    [BT] : add addFirst and toString to LinkedList class.

--- src/main/java/basics/programming/Main.java
+++ src/main/java/basics/programming/Main.java
@@ -1,7 +1,12 @@
 package basics.programming;
 
+import basics.programming.linkedlists.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LinkedList<Integer> list = new LinkedList<>();
+        list.addFirst(5);
+        list.addFirst(7);
+        System.out.println(list);
     }
 }
--- src/main/java/basics/programming/linkedlists/LinkedList.java
+++ src/main/java/basics/programming/linkedlists/LinkedList.java
@@ -1,4 +1,62 @@
 package basics.programming.linkedlists;
-public class LinkedList {
 
+public class LinkedList<T> {
+
+    private Node<T> first;
+    private Node<T> last;
+
+    public LinkedList() {
+    }
+
+    public LinkedList(Node<T> first, Node<T> last) {
+        this.first = first;
+        this.last = last;
+    }
+
+    public Node<T> getFirst() {
+        return first;
+    }
+
+    public void setFirst(Node<T> first) {
+        this.first = first;
+    }
+
+    public Node<T> getLast() {
+        return last;
+    }
+
+    public void setLast(Node<T> last) {
+        this.last = last;
+    }
+
+    // addFirst
+    public void addFirst(T element) {
+        Node<T> newNode = new Node<>(element, null);
+        if (this.first == null && this.last == null) {
+            this.first = this.last = newNode;
+        } else {
+            newNode.setNext(this.first);
+            this.first = newNode;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder("[ ");
+        Node<T> temp = this.first;
+        while (temp != null) {
+            output.append(temp.getValue());
+            temp = temp.getNext();
+            if (temp != null) {
+                output.append(", ");
+            }
+        }
+        return output.append(" ]").toString();
+    }
+
+    // addLast
+    // deleteFirst
+    // deleteLast
+    // contains
+    // indexOf
 }
